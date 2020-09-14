1b327b6be55a9682970cbe7e7464aad1bb37943f
kelli <kellikells@gmail.com>
2020-09-13 19:55:12 -0700

    took out empty jinja from templates, register and login routes work and add user to database

--- server.py
+++ server.py
@@ -8,11 +8,10 @@ import re   # "re"regular expression operations
 import pymysql
 import pymysql.cursors #makes data sent as python dictionaries
 
-mysql = connectToMySQL('registrationsdb')
+mysql = connectToMySQL('booksdb')
 
 # used for email validation
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-
 
 app = Flask(__name__) 
 bcrypt = Bcrypt(app)  
@@ -23,7 +22,7 @@ app.secret_key = "ThisIsSecret!"
 # ==============================================================
 @app.route('/')
 def index():
-    mysql = connectToMySQL('recipesdb')
+    mysql = connectToMySQL('booksdb')
     return render_template('index.html')
 
 # ==============================================================
@@ -49,7 +48,7 @@ def register():
 
     # checking if email already exists in db
     # --------------------------------------
-    mysql = connectToMySQL('recipesdb')
+    mysql = connectToMySQL('booksdb')
     query = "SELECT * FROM users WHERE email = %(email)s;"
     data = { "email" : request.form['email']}
     matchingEmail = mysql.query_db(query, data)
@@ -75,27 +74,26 @@ def register():
     # ADD NEW USER TO DATABASE : hash password
     # --------------------------------------
     else:
-        mysql = connectToMySQL('recipesdb')
+        mysql = connectToMySQL('booksdb')
         pw_hash = bcrypt.generate_password_hash(request.form['password']) 
 
-        query = "INSERT INTO users (user_name, email, password_hash, birthdate, created_at, updated_at) VALUES (%(user_name)s, %(email)s, %(password_hash)s, %(birthdate)s, NOW(), NOW());"
+        query = "INSERT INTO users (name, alias, email, password_hash, created_at, updated_at) VALUES (%(name)s, %(alias)s, %(email)s, %(password_hash)s, NOW(), NOW());"
         data = {
-             "user_name": request.form['username'],
+             "name": request.form['name'],
+             "alias": request.form['alias'],
              "email": request.form['email'],
-             "password_hash": pw_hash,
-             "birthdate": request.form['birthday']
+             "password_hash": pw_hash
         }
         new_user_id=mysql.query_db(query, data)
 
         # get user_id and store into session
         session['user_id'] = new_user_id
-        session['user_name'] = request.form['username']
+        session['user_alias'] = request.form['alias']
     
         print('SESSION:', session)
         flash("Aww yeah, you successfully registered.  You can now log in using the same information you provided!", 'success')
 
         return redirect('/')
-
 
 # ========================================================
 #                  LOGIN BUTTON ROUTE
@@ -105,7 +103,7 @@ def login():
     
     # check if email exists in database
     # --------------------------------------
-    mysql = connectToMySQL('recipesdb')
+    mysql = connectToMySQL('booksdb')
     query = "SELECT * FROM users WHERE email = %(email)s;"
     data = { "email" : request.form['email'] }
     result = mysql.query_db(query, data)
@@ -114,9 +112,10 @@ def login():
             
             # if True: store some user data in session
             session['user_id'] = result[0]['id']
-            session['user_name'] = result[0]['user_name']
-    
-            return redirect("/getReviews")
+            session['user_alias'] = result[0]['alias']
+
+            print(session)
+            return redirect('/getReviews')
     
         # if username & password don't match
         # --------------------------------------
@@ -130,100 +129,8 @@ def login():
 @app.route('/getReviews', methods=['GET'])
 def getReviews():
     user_id= session['user_id']
-
-
     return render_template('books.html', user_id = user_id)
 
-
-# # =====================================================
-# #                 DASHBOARD card : create
-# # =====================================================
-
-# @app.route('/create', methods=['GET'])
-# def create():
-#     user_id= session['user_id']
-#     return render_template('create.html', user_id = user_id)
-
-
-
-# # =====================================================
-# #                 DASHBOARD card : review
-# # =====================================================
-# @app.route('/review', methods=['GET'])
-# def review():
-#     user_id= session['user_id']
-
-#     mysql = connectToMySQL('recipesdb')
-#     query = "SELECT * FROM recipes WHERE user_id = %(user_id)s;"
-#     data = {
-#         "user_id": user_id
-#     }
-#     result = mysql.query_db(query, data)
-
-#     print("***** RESULT:", result)
-
-#     return render_template('review.html', user_id = user_id, recipes = result)
-
-
-# # =====================================================
-# #               button ROUTE: /createRecipe
-# # =====================================================
-# @app.route('/createRecipe', methods=['POST', 'GET'])
-# def createRecipe():
-#     user_id = session['user_id']
-
-#     mysql = connectToMySQL('recipesdb')
-#     query = "INSERT INTO recipes (name, description, instructions, cook_time, created_at, updated_at, user_id) VALUES (%(name)s, %(description)s, %(instructions)s, %(cook_time)s, NOW(), NOW(), %(user_id)s);"
-#     data = {
-#         "name": request.form['recipeName'],
-#         "description": request.form['description'],
-#         "instructions": request.form['instructions'],
-#         "cook_time": request.form['cook_time'],
-#         "user_id": user_id
-#     }
-
-#     mysql.query_db(query, data)
-
-#     flash("YOU CREATED A RECIPE, HOPE IT'S GOOD!", 'success')
-
-#     return redirect('/create')
-
-
-# # =====================================================
-# #                 DELETE A RECIPE
-# # =====================================================
-# @app.route('/delete', methods=['POST'])
-# def delete():
-#     mysql = connectToMySQL('recipesdb')
-#     query = "DELETE FROM recipes WHERE (id = %(id)s);"
-#     data = {
-#         'id': request.form['recipeID']
-#     }
-    
-#     mysql.query_db(query, data)
-
-#     return redirect('/review')
-
-
-
-# # =====================================================
-# #                 RECIPE INSTRUCTIONS
-# # =====================================================
-# @app.route('/instructions', methods=['POST'])
-# def instructions():
-#     user_id = session['user_id']
-
-#     mysql = connectToMySQL('recipesdb')
-#     query = "SELECT id, name, description, instructions, cook_time FROM recipes WHERE (recipes.id = %(id)s);"
-#     data = {
-#         'id': request.form['recipeID']
-#     }
-
-#     result = mysql.query_db(query, data)
-
-#     print('***** INSTRUCTIONS RESULT:', result)
-
-#     return render_template('instructions.html', recipe = result)
 
 
 # ====================================================
